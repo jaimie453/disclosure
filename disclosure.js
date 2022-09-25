9bf95f6b26a84fc2ac74a7cc52e0bae344ff31f6
Jaimie Bowen <jaimie6453@gmail.com>
2022-09-25 15:20:22 -0400

    setup navigableItems validation

--- disclosure.js
+++ disclosure.js
@@ -1,29 +1,40 @@
-
 /*
-    These requirements should also be present, but are not very practical to check, 
-    as it would rely too much on implementation details:
-        - button should have an accessible name
-        - no focusable elements should be between the button and the disclosure
+    CSS selector reference:
+        a[href],
+        area[href],
+        input:not([disabled]),
+        select:not([disabled]),
+        textarea:not([disabled]),
+        button:not([disabled]),
+        iframe,
+        [tabindex],
+        [contentEditable=true]
 */
-function checkDisclosureTriggerA11y(trigger) {
-    const triggerName = trigger.tagName.toLowerCase() + "#" + trigger.id;
-    if (trigger.tagName !== "BUTTON") {
-        console.warn(triggerName + " should be a button.");
-    }
-    if (!trigger.ariaExpanded) {
-        console.warn(triggerName + " is missing the 'aria-expanded' property.");
+
+{
+    const validElements = ["a", "area", "input", "select", "textarea", "button", "iframe"];
+
+    function isElementFocusable(element) {
+        return (validElements.includes(element.tagName.toLowerCase())
+            || element.hasAttribute("tabindex")
+            || element.contentEditable === 'true')
+            && !element.hasAttribute('disabled');
     }
 }
 
+
+// https://www.w3.org/WAI/ARIA/apg/patterns/disclosure/
 class disclosureEventHandler {
     trigger;    // the button element that opens disclosure
     disclosure; // the absolutely positioned disclosure element
     wrapper;    // the relatively positioned container element that contains both of the above
     onClose;
 
+    navigableItems;
+
     isListening = false;
 
-    constructor(disclosure, trigger, wrapper, onClose, navigableElements = []) {
+    constructor(disclosure, trigger, wrapper, onClose, navigableItems = []) {
         this.disclosure = disclosure;
         this.validateDisclosure();
 
@@ -35,6 +46,9 @@ class disclosureEventHandler {
 
         this.onClose = onClose;
         this.validateOnClose();
+
+        this.navigableItems = navigableItems;
+        this.validateNavigableItems();
     }
 
     handleClickOutside = (event) => {
@@ -44,52 +58,54 @@ class disclosureEventHandler {
 
     /* close if focus leaves disclosure or trigger button */
     handleFocusOutside = (event) => {
-        const isFocusedElementInsideDisclosure = this.disclosure.contains(event.relatedTarget);
-        const isTriggerFocused = event.relatedTarget === this.trigger;
+        if(!event.relatedTarget) return;
+        
+        const isFocusInsideDisclosure = this.disclosure.contains(event.relatedTarget) 
+                                        || event.relatedTarget === this.trigger;
 
-        if (!isFocusedElementInsideDisclosure && !isTriggerFocused) this.removeEventListeners();
+        if (!isFocusInsideDisclosure) this.removeEventListeners();
+        else console.log("focus is inside disclosure");// need to check for navigableItems focus so index stays consistent
     }
 
-    // /*  Escape
-    //         closes menu and reverts focus to right split button
-    //     Arrow keys
-    //         allows for restricted navigation (cannot leave disclosure) between disclosure items
-    // */
-    // //const disclosureButtons = disclosure.getElementsByTagName('button'); // would need to modify this if links are used
-    // handleKeyInput(event) {
-    //     switch (event.key) {
-    //         case 'Escape':
-    //             $actions.Closedisclosure();
-    //             button.focus();
-    //             removeEvents();
-    //             break;
-    //         /*
-    //         case 'ArrowUp':  
-    //             event.preventDefault();
+    /*  Escape
+            closes menu and reverts focus to right split button
+        Arrow keys
+            allows for restricted navigation (cannot leave disclosure) between disclosure items
+    */
+    handleKeyInput = (event) => {
+        if (event.key === 'Escape') {
+            this.removeEventListeners();
+            this.trigger.focus();
+        }
 
-    //             if(buttonIndex > 0) {
-    //                 --buttonIndex
-    //             } else {
-    //                 buttonIndex = disclosureButtons.length - 1
-    //             }
+        if (false) {
+            switch (event.key) {
+                case 'ArrowUp' || 'ArrowLeft':
+                    event.preventDefault();
 
-    //             disclosureButtons[buttonIndex].focus();
-    //             break;
+                    if (buttonIndex > 0) {
+                        --buttonIndex
+                    } else {
+                        buttonIndex = disclosureButtons.length - 1
+                    }
 
-    //         case 'ArrowDown': 
-    //             event.preventDefault();
+                    disclosureButtons[buttonIndex].focus();
+                    break;
 
-    //             if(buttonIndex < disclosureButtons.length - 1) {
-    //                 ++buttonIndex 
-    //             } else {
-    //                 buttonIndex = 0;
-    //             }
+                case 'ArrowDown' || 'ArrowRight':
+                    event.preventDefault();
 
-    //             disclosureButtons[buttonIndex].focus();
-    //             break;    
-    //             */
-    //     }
-    // }
+                    if (buttonIndex < disclosureButtons.length - 1) {
+                        ++buttonIndex
+                    } else {
+                        buttonIndex = 0;
+                    }
+
+                    disclosureButtons[buttonIndex].focus();
+                    break;
+            }
+        }
+    }
 
     toggleEventListeners = () => {
         if (this.isListening) this.removeEventListeners();
@@ -97,22 +113,27 @@ class disclosureEventHandler {
     }
 
     removeEventListeners = () => {
-        this.isListening = false;
+        if (!this.isListening) return;
+
         document.removeEventListener('click', this.handleClickOutside);
         wrapper.removeEventListener('focusout', this.handleFocusOutside);
-        //document.removeEventListener('keydown', onKeyDown);
+        document.removeEventListener('keydown', this.handleKeyInput);
 
+        this.isListening = false;
         this.onClose();
     }
 
     addEventListeners = () => {
-        /* needed otherwise the button will instantly close */
-        setTimeout(() => {
-            this.isListening = true;
+        if (this.isListening) return;
+
+        /* needed otherwise the button will instantly close on click */
+        setTimeout(() => {  
             document.addEventListener('click', this.handleClickOutside);
             wrapper.addEventListener('focusout', this.handleFocusOutside);
-            //document.addEventListener("keydown", onKeyDown);
+            document.addEventListener("keydown", this.handleKeyInput);   
         }, 50);
+
+        this.isListening = true;
     }
 
     validateDisclosure = () => {
@@ -120,13 +141,24 @@ class disclosureEventHandler {
         if (!(this.disclosure instanceof HTMLElement)) throw new Error("disclosure must be an HTMLElement");
     }
 
+    /*
+        These a11y requirements should also be present, but are not very 
+        practical to check, as it would rely too much on implementation details:
+            - button should have an accessible name
+            - no focusable elements should be between the button and the disclosure
+    */
     validateTrigger = () => {
         if (!this.trigger) throw new Error("trigger is null");
         if (!(this.trigger instanceof HTMLElement)) throw new Error("trigger must be an HTMLElement");
-        checkDisclosureTriggerA11y(this.trigger);
+        if (this.disclosure.contains(this.trigger)) throw new Error("trigger cannot be inside disclosure");
+
+        const triggerName = this.trigger.tagName.toLowerCase() + "#" + this.trigger.id;
+        if (this.trigger.tagName !== "BUTTON") console.warn(triggerName + " should be a button.");
+        if (!this.trigger.ariaExpanded) console.warn(triggerName + " is missing the 'aria-expanded' property.");
     }
 
     validateWrapper = () => {
+        if (!this.wrapper) throw new Error("wrapper is null");
         if (!(this.wrapper instanceof HTMLElement)) throw new Error("wrapper must be an HTMLElement");
         if (!this.wrapper.contains(this.disclosure) || !this.wrapper.contains(this.trigger)) {
             throw new Error("wrapper must contain trigger and disclosure");
@@ -137,6 +169,22 @@ class disclosureEventHandler {
         if (!this.onClose) throw new Error("onClose is null");
         if (typeof this.onClose !== 'function') throw new Error("onClose must be a function");
     }
+
+    validateNavigableItems = () => {
+        if (!this.navigableItems) throw new Error("navigableItems is null");
+        if (!(this.navigableItems instanceof HTMLCollection)) throw new Error("navigableItems must be an HTMLCollection");
+
+        let showButtonLinkWarning = true;
+        Array.from(navigableItems).forEach(navigableItem => {
+            if (!this.disclosure.contains(navigableItem)) throw new Error("navigableItems must be inside disclosure");
+            if (!isElementFocusable(navigableItem)) throw new Error("navigableItems contains non focusable elements");
+
+            if (showButtonLinkWarning && (navigableItem.tagName !== 'BUTTON' && navigableItem.tagName !== 'A')) {
+                console.warn("disclosureEventHandler: navigableItems should only contain buttons and/or links");
+                showButtonLinkWarning = false;
+            }
+        });
+    }
 }
 
 
@@ -144,6 +192,7 @@ class disclosureEventHandler {
 const trigger = document.getElementById("trigger");
 const disclosure = document.getElementById("disclosure");
 const wrapper = document.getElementById("wrapper");
+const navigableItems = document.getElementsByClassName("focus-test");
 
 const showDisclosure = () => {
     disclosure.style.display = '';
@@ -157,7 +206,7 @@ const hideDisclosure = () => {
 
 const disclosureHandler = new disclosureEventHandler(disclosure, trigger, wrapper, onClose = () => {
     hideDisclosure();
-});
+}, navigableItems);
 
 const toggle = () => {
     if (disclosure.style.display === 'none') showDisclosure();
